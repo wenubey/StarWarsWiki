8ce1bf9265827f3efc84b746906e383a355e81ee
wenubey <mertfatihsimsek06@gmail.com>
2023-05-16 22:03:16 +0200

    start to create CharacterDetailScreen

--- app/src/main/java/com/wenubey/starwarswiki/core/extensionFunctions.kt
+++ app/src/main/java/com/wenubey/starwarswiki/core/extensionFunctions.kt
@@ -1,5 +1,9 @@
 package com.wenubey.starwarswiki.core
 
+import android.content.Intent
+import android.os.Build.VERSION.SDK_INT
+import android.os.Bundle
+import android.os.Parcelable
 import com.wenubey.starwarswiki.core.Constants.UNDEFINED
 import com.wenubey.starwarswiki.domain.models.PlanetModel
 import com.wenubey.starwarswiki.domain.models.SpecieModel
@@ -19,5 +23,14 @@ fun List<SpecieModel>?.getFirstOrNull(): String {
     }
 }
 
+inline fun <reified T : Parcelable> Intent.parcelable(key: String): T? = when {
+    SDK_INT >= 33 -> getParcelableExtra(key, T::class.java)
+    else -> @Suppress("DEPRECATION") getParcelableExtra(key) as? T
+}
+
+inline fun <reified T : Parcelable> Bundle.parcelable(key: String): T? = when {
+    SDK_INT >= 33 -> getParcelable(key, T::class.java)
+    else -> @Suppress("DEPRECATION") getParcelable(key) as? T
+}
 
 
