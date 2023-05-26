eb487f7f81011146c6120a35391b04a133be5318
wenubey <mertfatihsimsek06@gmail.com>
2023-05-26 22:13:31 +0200

    remove starwars.xml and added assets folder and Starfield.png and added animations

--- app/src/main/java/com/wenubey/starwarswiki/presentation/screens/OpeningCrawlScreen.kt
+++ app/src/main/java/com/wenubey/starwarswiki/presentation/screens/OpeningCrawlScreen.kt
@@ -1,16 +1,18 @@
 package com.wenubey.starwarswiki.presentation.screens
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.keyframes
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,16 +23,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.wenubey.starwarswiki.R
+import coil.compose.AsyncImage
+import com.wenubey.starwarswiki.core.Constants.TAG
 import com.wenubey.starwarswiki.core.components.ErrorScreen
 import com.wenubey.starwarswiki.core.components.StarWarsTopBarWithBackButton
 import com.wenubey.starwarswiki.domain.models.FilmModel
@@ -41,28 +44,36 @@ fun OpeningCrawlScreen(
     film: FilmModel?,
     navigateToBackScreen: () -> Unit,
 ) {
-    var offsetY by remember { mutableStateOf(0.dp) }
+
+    var offsetY by remember { mutableStateOf(100.dp) }
+    var boxVisibility by remember { mutableStateOf(true) }
+    val context = LocalContext.current
+    val assetManager = context.assets
 
     LaunchedEffect(Unit) {
         val animationSpec = keyframes {
-            durationMillis = 4000
-            0f at 4000
-            0.25f at 3000
-            0.50f at 2000
-            0.75f at 1000
+            durationMillis = 12000
             1f at 0
+            0.50f at 6000
+            0f at 12000
+            //-0.5f at 18000
+            // -1f at 24000
+
         }
-
-        val offsetAnim = Animatable(0f)
-
+        val offsetAnim = Animatable(1f)
         offsetAnim.animateTo(
             targetValue = 0f,
-            animationSpec = animationSpec
+            animationSpec = animationSpec,
         ) {
             offsetY = (value * 500).dp
+            // Log.i(TAG, "OpeningCrawlScreen: offset: $offsetY")
+            Log.i(TAG, "OpeningCrawlScreen: value: $value")
         }
+        boxVisibility = false
     }
 
+    val imgFile = assetManager.open("Starfield.png")
+    val imgBitmap: Bitmap = BitmapFactory.decodeStream(imgFile)
 
     Scaffold(
         modifier = Modifier,
@@ -70,54 +81,59 @@ fun OpeningCrawlScreen(
             StarWarsTopBarWithBackButton(navigateToBackScreen = navigateToBackScreen)
         },
         content = { paddingValues ->
-            Image(
-                modifier = Modifier.fillMaxSize(),
-                painter = painterResource(id = R.drawable.starwars),
+            AsyncImage(
+                model = imgBitmap,
                 contentDescription = "",
                 contentScale = ContentScale.FillBounds,
+                modifier = Modifier.fillMaxSize()
             )
             if (film != null && !film.openingCrawl.isNullOrEmpty() && !film.title.isNullOrEmpty()) {
                 Box(
                     modifier = Modifier
                         .fillMaxSize()
-                        .padding(paddingValues)
-                        .background(Color.Transparent)
-                        .padding(4.dp)
-                        .wrapContentHeight()
+                        .graphicsLayer {
+                            rotationX = 15f
+                            transformOrigin = TransformOrigin(0.5f, 0.8f)
+                            scaleY = 2f
+                        }
                         .offset(y = offsetY),
+                    contentAlignment = Alignment.TopCenter
                 ) {
-
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.spacedBy(4.dp),
+                    AnimatedVisibility(
+                        visible = boxVisibility,
+                        exit = shrinkVertically()
                     ) {
-                        Text(
-                            text = film.title,
-                            fontSize = 36.sp,
-                            color = Color.Yellow,
-                            textAlign = TextAlign.Center,
-                            style = TextStyle.Default.copy(
-                                letterSpacing = 2.sp
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.spacedBy(4.dp),
+                        ) {
+                            Text(
+                                text = film.title,
+                                fontSize = 36.sp,
+                                color = Color.Yellow,
+                                textAlign = TextAlign.Center,
+                                style = TextStyle.Default.copy(
+                                    letterSpacing = 2.sp
+                                )
                             )
-                        )
-                        Text(
-                            text = film.openingCrawl,
-                            modifier = Modifier
-                                .padding(16.dp),
-                            fontSize = 24.sp,
-                            textAlign = TextAlign.Start,
-                            color = Color.Yellow
-                        )
+                            Text(
+                                text = film.openingCrawl,
+                                modifier = Modifier
+                                    .padding(16.dp),
+                                fontSize = 24.sp,
+                                textAlign = TextAlign.Center,
+                                color = Color.Yellow
+                            )
+                        }
                     }
-
                 }
+
             } else {
                 ErrorScreen(paddingValues = paddingValues)
             }
 
         }
     )
-
 
 }
 
