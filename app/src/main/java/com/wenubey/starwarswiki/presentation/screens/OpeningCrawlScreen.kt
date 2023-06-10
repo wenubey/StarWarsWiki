d4b48b73498ec3c5a47b2979bb3d768fc1f00d96
wenubey <mertfatihsimsek06@gmail.com>
2023-06-10 12:04:49 +0200

    OpeningCrawlScreen is now have a landscape orientation. And OpeningCrawlScreen refactor the code for optimization.

--- app/src/main/java/com/wenubey/starwarswiki/presentation/screens/OpeningCrawlScreen.kt
+++ app/src/main/java/com/wenubey/starwarswiki/presentation/screens/OpeningCrawlScreen.kt
@@ -1,18 +1,22 @@
 package com.wenubey.starwarswiki.presentation.screens
 
+import android.content.res.Configuration
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.animateScrollBy
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -28,6 +32,7 @@ import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -36,6 +41,7 @@ import com.wenubey.starwarswiki.core.components.ErrorScreen
 import com.wenubey.starwarswiki.core.components.StarWarsTopBar
 import com.wenubey.starwarswiki.domain.models.FilmModel
 import com.wenubey.starwarswiki.presentation.ui.theme.OpeningCrawlColor
+import kotlinx.coroutines.launch
 
 
 @Composable
@@ -43,33 +49,59 @@ fun OpeningCrawlScreen(
     film: FilmModel?,
     navigateToBackScreen: () -> Unit,
 ) {
-
-    var offsetY by remember { mutableStateOf(100.dp) }
+    var offsetY by remember { mutableStateOf(0.dp) }
     var boxVisibility by remember { mutableStateOf(true) }
+    val lazyListState = rememberLazyListState()
+    val configuration = LocalConfiguration.current
+    val isPortrait by remember {
+        mutableStateOf(configuration.orientation == Configuration.ORIENTATION_PORTRAIT)
+    }
+
     val context = LocalContext.current
     val assetManager = context.assets
     val imgFile = assetManager.open("star_field.png")
     val imgBitmap: Bitmap = BitmapFactory.decodeStream(imgFile)
 
-    LaunchedEffect(Unit) {
-        val animationSpec = keyframes {
-            durationMillis = 24000
-            1f at 0
-            0.50f at 4000
-            0f at 8000
-            -0.5f at 12000
-            -1f at 16000
-            -1.5f at 20000
-            -2f at 24000
+    if (isPortrait) {
+        LaunchedEffect(Unit) {
+            launch {
+                lazyListState.scrollToItem(0)
+                val animationSpec = keyframes {
+                    durationMillis = 24000
+                    1f at 0
+                    0.50f at 4000
+                    0f at 8000
+                    -0.5f at 12000
+                    -1f at 16000
+                    -1.5f at 20000
+                    -2f at 24000
+                }
+                val offsetAnim = Animatable(1f)
+                offsetAnim.animateTo(
+                    targetValue = -2f,
+                    animationSpec = animationSpec,
+                ) {
+                    offsetY = (value * 500).dp
+                }
+                boxVisibility = false
+            }
         }
-        val offsetAnim = Animatable(1f)
-        offsetAnim.animateTo(
-            targetValue = -2f,
-            animationSpec = animationSpec,
-        ) {
-            offsetY = (value * 500).dp
+    } else {
+        LaunchedEffect(Unit) {
+            launch {
+                val lazyListHeight =
+                    lazyListState.layoutInfo.viewportEndOffset.toFloat() * lazyListState.layoutInfo.totalItemsCount
+                lazyListState.animateScrollBy(
+                    value = lazyListHeight,
+                    animationSpec = tween(
+                        durationMillis = 48000,
+                        easing = LinearEasing,
+                        delayMillis = 500
+                    )
+                )
+            }
+            boxVisibility = false
         }
-        boxVisibility = false
     }
 
     Scaffold(
@@ -87,41 +119,75 @@ fun OpeningCrawlScreen(
                 modifier = Modifier.fillMaxSize()
             )
             if (film != null && !film.openingCrawl.isNullOrEmpty() && !film.title.isNullOrEmpty()) {
-                Box(
-                    modifier = Modifier
+                AnimatedVisibility(visible = boxVisibility, exit = shrinkVertically()) {
+                    LazyColumn(modifier = Modifier
                         .fillMaxSize()
                         .padding(paddingValues)
                         .graphicsLayer {
                             rotationX = 15f
                             transformOrigin = TransformOrigin(0.5f, 0.8f)
-                            scaleY = 2f
+                            scaleY = 1f
                         }
                         .offset(y = offsetY),
-                    contentAlignment = Alignment.TopCenter
-                ) {
-                    AnimatedVisibility(
-                        visible = boxVisibility,
-                        exit = shrinkVertically()
+                        state = lazyListState,
+                        verticalArrangement = Arrangement.spacedBy(2.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        Column(
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.spacedBy(4.dp),
-                        ) {
+                        item {
                             Text(
                                 text = film.title,
                                 color = OpeningCrawlColor,
                                 textAlign = TextAlign.Center,
                                 style = MaterialTheme.typography.headlineLarge
                             )
+                        }
+                        val openingCrawlLines = film.openingCrawl.split("\r\n")
+                        items(openingCrawlLines.size) { index ->
                             Text(
-                                text = film.openingCrawl,
+                                text = openingCrawlLines[index],
                                 textAlign = TextAlign.Center,
                                 color = OpeningCrawlColor,
-                                style = MaterialTheme.typography.bodyLarge
+                                style = if (isPortrait) MaterialTheme.typography.headlineMedium else MaterialTheme.typography.headlineLarge
                             )
                         }
                     }
                 }
+//                Box(
+//                    modifier = Modifier
+//                        .fillMaxSize()
+//                        .padding(paddingValues)
+//                        .graphicsLayer {
+//                            rotationX = 15f
+//                            transformOrigin = TransformOrigin(0.5f, 0.8f)
+//                            scaleY = 1f
+//                        }
+//                        .offset(y = offsetY),
+//                    contentAlignment = Alignment.TopCenter
+//                ) {
+//                    AnimatedVisibility(
+//                        visible = boxVisibility,
+//                        exit = shrinkVertically(),
+//                    ) {
+//                        Column(
+//                            modifier = Modifier.fillMaxSize(),
+//                            horizontalAlignment = Alignment.CenterHorizontally,
+//                            verticalArrangement = Arrangement.spacedBy(4.dp),
+//                        ) {
+//                            Text(
+//                                text = film.title,
+//                                color = OpeningCrawlColor,
+//                                textAlign = TextAlign.Center,
+//                                style = MaterialTheme.typography.headlineLarge
+//                            )
+//                            Text(
+//                                text = film.openingCrawl,
+//                                textAlign = TextAlign.Center,
+//                                color = OpeningCrawlColor,
+//                                style = MaterialTheme.typography.bodyMedium
+//                            )
+//                        }
+//                    }
+//                }
 
             } else {
                 ErrorScreen()
--- app/src/main/java/com/wenubey/starwarswiki/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/wenubey/starwarswiki/presentation/ui/theme/Type.kt
@@ -1,56 +1,79 @@
 package com.wenubey.starwarswiki.presentation.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineBreak
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.sp
 import com.wenubey.starwarswiki.R
 
 
-val gothicA1Font = FontFamily(
+private val GothicA1Font = FontFamily(
     listOf(
         Font(R.font.gothic_a1_black),
-        Font(R.font.gothic_a1_bold),
-        Font(R.font.gothic_a1_extra_bold),
+        Font(R.font.gothic_a1_bold, weight = FontWeight.Bold),
+        Font(R.font.gothic_a1_extra_bold, weight = FontWeight.ExtraBold),
         Font(R.font.gothic_a1_extra_light),
         Font(R.font.gothic_a1_light),
-        Font(R.font.gothic_a1_medium),
+        Font(R.font.gothic_a1_medium, FontWeight.W500),
         Font(R.font.gothic_a1_regular),
         Font(R.font.gothic_a1_semi_bold),
     )
 )
+
+@Suppress("DEPRECATION")
+val defaultTextStyle = TextStyle(
+    fontFamily = GothicA1Font,
+    platformStyle = PlatformTextStyle(
+        includeFontPadding = false
+    ),
+    lineHeightStyle = LineHeightStyle(
+        alignment = LineHeightStyle.Alignment.Center,
+        trim = LineHeightStyle.Trim.None
+    )
+)
+
 // Set of Material typography styles to start with
 val Typography = Typography(
+
     bodyLarge = TextStyle(
-        fontFamily = gothicA1Font,
+        fontFamily = GothicA1Font,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.sp
     ),
     bodyMedium = TextStyle(
-        fontFamily = gothicA1Font,
+        fontFamily = GothicA1Font,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp,
         lineHeight = 12.sp,
         letterSpacing = 0.sp
     ),
     headlineLarge = TextStyle(
-        fontFamily = gothicA1Font,
+        fontFamily = GothicA1Font,
         fontWeight = FontWeight.Bold,
         fontSize = 36.sp,
         lineHeight = 36.sp,
         letterSpacing = 1.sp
     ),
-    headlineMedium = TextStyle(
-        fontFamily = gothicA1Font,
-        fontWeight = FontWeight.SemiBold,
+    headlineMedium = defaultTextStyle.copy(
         fontSize = 20.sp,
-        lineHeight = 20.sp,
-        letterSpacing = 1.sp
+        lineHeight = 24.sp,
+        letterSpacing = 0.sp,
+        lineBreak = LineBreak.Heading
     )
+//    TextStyle(
+//        fontFamily = GothicA1Font,
+//        fontWeight = FontWeight.SemiBold,
+//        fontSize = 20.sp,
+//        lineHeight = 20.sp,
+//        letterSpacing = 1.sp
+//    )
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
